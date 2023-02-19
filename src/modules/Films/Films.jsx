2769ca1b7659bfe43ab9edb5a0fc32c2026348f1
ValentinaDej <safonikv@gmail.com>
2023-02-19 14:34:43 +0200

    Update Films.jsx

--- src/modules/Films/Films.jsx
+++ src/modules/Films/Films.jsx
@@ -4,54 +4,30 @@ import { getDailyPopularFilms } from '../../shared/Services/filmApi';
 import FilmsList from 'modules/FilmsList/FilmsList';
 
 const Films = () => {
-  const [state, setState] = useState({
-    items: [],
-    // loading: false,
-    error: null,
-  });
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchFilms = async () => {
-      setState({
-        ...state,
-        // loading: true,
-        error: null,
-      });
-
       try {
         const result = await getDailyPopularFilms();
-        setState(prevState => {
-          return {
-            ...prevState,
-            items: [...result],
-          };
-        });
+        setItems([...result]);
+        setLoading(true);
       } catch (error) {
-        setState(prevState => {
-          return {
-            ...prevState,
-            error,
-          };
-        });
+        setError(error.message);
       } finally {
-        setState(prevState => {
-          return {
-            ...prevState,
-            // loading: false,
-          };
-        });
+        setLoading(false);
       }
     };
 
     fetchFilms();
-  }, [state]);
-
-  const { items, error } = state;
+  }, [setItems, setLoading, setError]);
 
   return (
     <div>
       {items.length > 0 && <FilmsList items={items} />}
-      {/* {loading && <p>...loading films</p>} */}
+      {loading && <p>...loading films</p>}
       {error && <p>...films load faild</p>}
     </div>
   );
